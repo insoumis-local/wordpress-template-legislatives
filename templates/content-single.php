f1fb8df89e3cb46e8af30dadb49ebd9970da3131
Nico Prat <nicooprat@gmail.com>
2017-05-13 17:19:03 +0200

    Removed thumbnail in post detail (fix #4)

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,10 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <?php if (has_post_thumbnail() && get_post_format() !== 'video') {
-        the_post_thumbnail('large');
-        echo '<p><em class="text-muted"><small>'.get_post(get_post_thumbnail_id())->post_excerpt.'</small></em></p>';
-      } ?>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
