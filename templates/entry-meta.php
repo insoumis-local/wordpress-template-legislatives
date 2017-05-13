8e8c5c58284c2c0e2f08b37c28c294262d163412
Nico Prat <nicooprat@gmail.com>
2017-05-13 17:47:18 +0200

    Don't display default category  on posts lists

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,13 +1,13 @@
-<span class="text-muted">
-<time class="updated" datetime="<?php echo get_post_time('c', true); ?>"><?php echo get_the_date(); ?></time>
-dans
-<?php
-  $categories = get_the_category();
+<p class="text-muted entry-meta">
+  <time class="updated" datetime="<?php echo get_post_time('c', true); ?>"><?php echo get_the_date(); ?></time>
+  <?php
+    $categories = get_the_category();
 
-  foreach ($categories as $key => $category) {
-?>
-<a href="<?php echo get_category_link($category->cat_ID) ?>">
-  <?php echo $category->name; ?>
-</a><?php echo $key < count($categories) - 1 ? ', ': '' ?>
-<?php } ?>
-</span>
+    foreach ($categories as $key => $category) {
+      if($category->cat_ID != 1) {
+  ?>
+  <a href="<?php echo get_category_link($category->cat_ID) ?>">
+    <?php echo $category->name; ?>
+  </a>
+  <?php } } ?>
+</p>
