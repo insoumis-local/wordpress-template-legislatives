b26bb5e0e181df300f41403435bf381ba12d1e52
Nico Prat <nicooprat@gmail.com>
2017-05-13 17:14:12 +0200

    Added favicon

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,10 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('stylesheet_directory'); ?>/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('stylesheet_directory'); ?>/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('stylesheet_directory'); ?>/favicons/favicon-16x16.png">
+  <link rel="manifest" href="<?php bloginfo('stylesheet_directory'); ?>/favicons/manifest.json">
+  <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
 </head>
